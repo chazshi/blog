571016a9114909c8122a7dd5950f456dd61a5779
chazshi <sczhi55@gmail.com>
2016-01-10 05:06:12 +0800

    block1 completed

---
+++ js/Hozplayer.js
@@ -0,0 +1,8 @@
+var hozplayer = function myplayer(){
+	var listBtn = document.getElementsByClassName("listBtn");
+	listBtn.onclick = function(){
+		var listArea = document.getElementsByClassName("listArea");
+		listArea.style.display = 'none';
+		alert("fk");
+	};
+};
